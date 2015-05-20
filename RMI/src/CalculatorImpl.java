6fd0e1f96e544afc5cc74cc33083f4ee4b75972d
Ashesh Kumar <user501254@gmail.com>
2015-05-20 22:54:47 +0530

    Implementation of Java RMI

---
+++ RMI/src/CalculatorImpl.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ashesh
+ */
+public class CalculatorImpl
+        extends
+        java.rmi.server.UnicastRemoteObject
+        implements Calculator {
+
+  // Implementations must have an 
+  //explicit constructor 
+  // in order to declare the 
+  //RemoteException exception 
+  public CalculatorImpl()
+          throws java.rmi.RemoteException {
+    super();
+  }
+
+  public long add(long a, long b)
+          throws java.rmi.RemoteException {
+    return a + b;
+  }
+
+  public long sub(long a, long b)
+          throws java.rmi.RemoteException {
+    return a - b;
+  }
+
+  public long mul(long a, long b)
+          throws java.rmi.RemoteException {
+    return a * b;
+  }
+
+  public long div(long a, long b)
+          throws java.rmi.RemoteException {
+    return a / b;
+  }
+}
